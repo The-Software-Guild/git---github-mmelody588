820b28fcbd3cfdb29061e899b8e44c99e54547ce
Martin Melody <mmelody588@gmail.com>
2021-07-12 11:31:09 -0500

    Hello World in NetBeans

---
+++ classwork/HelloWorld/src/main/java/com/mm/helloworld/Hello.java
@@ -0,0 +1,13 @@
+/*
+ * @author: Martin Melody
+ * email: mmelody588@gmail.com
+ * date: 07/12/2021
+*  purpose: My first Hello World program in NetBeans IDE
+ */
+package com.mm.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Martin!");
+    }
+}
